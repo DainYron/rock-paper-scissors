ec2eed7c0f0dad71badaf077ef77deeb1769a577
DainYron <158751600+DainYron@users.noreply.github.com>
2024-07-05 13:56:44 +0530

    Finished Game with visual indicators.

--- rps.js
+++ rps.js
@@ -8,7 +8,7 @@ var computerSelection;
 var score = 0;
 var lives = 5;
 var streak = 0;
-
+var currentStreak = 0;
 function playGame() {
 
     const rockButton = document.querySelector(".rock");
@@ -47,17 +47,19 @@ function createButtons() {
 function onClick(playerSelection) {
     
     
-    const res = document.createElement("div");
+        const res = document.createElement("div");
         computerSelection = computerSelect();
-        res.textContent = "You chose: " + playerSelection + " " + "Computer chose: " + computerSelection;
-        body.appendChild(res);
+        resetColors();
+        //res.textContent = "You chose: " + playerSelection + " " + "Computer chose: " + computerSelection;
+        //body.appendChild(res);
         var round = checkScore(playerSelection, computerSelection);
-        //updateColors(button, playerSelection, computerSelection, round);
+        updateColors(playerSelection, computerSelection, round);
         const rep = document.createElement("div");
-        rep.textContent = "Score: " + round;
-        body.appendChild(rep);
+        //rep.textContent = "Score: " + round;
+        //body.appendChild(rep);
         updateScore(round);
         updateText();
+        
         
         if (lives <=0 ){
             deleteButtons();
@@ -94,45 +96,68 @@ function checkScore(playerSelection, computerSelection) {
 function updateScore(round){
   if (round == 1) {
     score++;
-    streak++;
+    currentStreak++;
+    streak = Math.max(streak, currentStreak);
   }
   if (round == -1){
     lives--;
-    streak = 0;
+    currentStreak = 0;
   }
-  if (round == 0){
-    streak = 0;
-  }
+  
 }
 function updateText() {
   scoreSheet.innerText = `Score: ${score} Lives: ${lives} Streak: ${streak}`;
 }
 
-function updateColors(button ,playerSelection, computerSelection, round){
-    if (round > 0) {
-        if (button.classList.contains(playerSelection)){
-            button.classList.add("correct");
+function updateColors(playerSelection, computerSelection, round){
+
+    console.log(1);
+    console.log(playerSelection, computerSelection, round);
+    const rockButton = document.querySelector(".rock");
+    const paperButton = document.querySelector(".paper");
+    const scissorsButton = document.querySelector(".scissors");
+    
+    if (round == 1) {
+        if (playerSelection == "rock") {
+            rockButton.style.background='#00FF00';
         }
-        if (button.classList.contains(computerSelection)){
-            button.classList.add("incorrect");
+        if (playerSelection == "paper") {
+            paperButton.style.background='#00FF00';
         }
-    }
-    if (round == 0) {
-        if (button.classList.contains(playerSelection)){
-            button.classList.add("neither");
-        }
-        if (button.classList.contains(computerSelection)){
-            button.classList.add("neither");
-        }
-    }
-    if (round < 0) {
-        if (button.classList.contains(playerSelection)){
-            button.classList.add("incorrect");
-        }
-        if (button.classList.contains(computerSelection)){
-            button.classList.add("correct");
+        if (playerSelection == "scissors") {
+            scissorsButton.style.background='#00FF00';
         }
     }
+    else if (round == -1) {
+        if (playerSelection == "rock") {
+            rockButton.style.background='#FF0000';
+        }
+        if (playerSelection == "paper") {
+            paperButton.style.background='#FF0000';
+        }
+        if (playerSelection == "scissors") {
+            scissorsButton.style.background='#FF0000';
+        }
+    }
+    
+    if (computerSelection == "rock") {
+        rockButton.style.background='#0000FF';
+    }
+    else if (computerSelection == "paper") {
+        paperButton.style.background='#0000FF';
+    }
+    else if (computerSelection == "scissors") {
+        scissorsButton.style.background='#0000FF';
+    }
+}
+
+function resetColors(button){
+    const rockButton = document.querySelector(".rock");
+    const paperButton = document.querySelector(".paper");
+    const scissorsButton = document.querySelector(".scissors");
+    rockButton.style.background='#FFFFFF';
+    paperButton.style.background='#FFFFFF';
+    scissorsButton.style.background='#FFFFFF';
 }
 
 function deleteButtons() {
@@ -160,6 +185,7 @@ function gameStart(){
     
     const playAgain = document.querySelector(".playAgain");
     start.removeChild(playAgain);
+    updateText();
     const startButton = document.createElement("button");
     startButton.classList.add("choices");
     startButton.setAttribute("id", "button");
